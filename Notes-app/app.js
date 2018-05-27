abac3c40eadd32a3933848cb928046add841afde
joaopppobr <joaopppobr@gmail.com>
2018-05-26 23:40:01 -0300

    updates

--- Notes-app/app.js
+++ Notes-app/app.js
@@ -1,20 +1,25 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-//var user = os.userInfo();
-var res = notes.addNote();
-
-var filteredArray = _.uniq(['andrew', 'john', 'bob']);
-console.log(filteredArray);
-//console.log(_.isString(res))
-//console.log(res);
-
-//fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err) {
-//  if (err){
-//    console.log('Unable to write file.');
-//  }
-//});
+const argv = yargs.argv;
+var command = argv._[0];
+if (command == 'add'){
+  notes.addNote(argv.title, argv.body);
+}
+else if (command == 'list'){
+  notes.getAll();
+}
+else if (command == 'read'){
+  notes.getNote(argv.title);
+}
+else if (command == 'remove') {
+  notes.readNote(argv.title);
+}
+else {
+  console.log("Command not recognized");
+}
--- Notes-app/notes.js
+++ Notes-app/notes.js
@@ -1,10 +1,43 @@
 console.log('Starting notes.js');
+const fs = require('fs');
 
-module.exports.addNote = function() {
-  console.log('addNote');
-  return 'New Note';
+var addNote = function(title, body) {
+  var notes = [];
+  var note = {
+    title,
+    body
+  };
+
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch (e) {
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+
 };
 
-module.exports.add = function(a, b) {
-  return a+b;
-}
+var getAll = function() {
+  console.log('Getting all notes');
+};
+var getNote = function(title) {
+  console.log('Reading note', title);
+};
+
+var removeNote = function(title) {
+  console.log('removing note note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
